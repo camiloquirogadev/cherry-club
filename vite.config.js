e2bcf89667b0507d4729fc53ac48cbae9f86bfa8
Camilo Quiroga <quirogacamilodev@gmail.com>
2025-04-08 14:10:13 -0300

    Agrego página 404 y ajustes para despliegue

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/cherry-club/',
-  plugins: [react()]
-})
+  base: "/cherry-club/",
+  plugins: [react()],
+});
